c6afae351b3426267b4eb8a5d58a57de502374ca
Jillian Guillen <JGuillen@spartaglobal.com>
2019-10-03 12:37:55 +0100

    did pseudo code

---
+++ db_class.py
@@ -0,0 +1,10 @@
+# Give it the necessary attributes
+# Start defining methods
+
+
+# all() # have this one the db_class
+    # gets all the instances from DB
+        #get each record
+        #create individual instances of recipe
+        #store them in a list
+        #return_list
---
+++ recipe_class.py
@@ -0,0 +1,14 @@
+# Define a class recipe
+
+# new()
+    # create a recipe object
+
+# save()
+    # saves a recipe objtect to the DB (make it persistent)
+
+# read()
+    # reads one object
+
+
+# Destroy
+    # one object 
