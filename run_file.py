040dca1ed1b74644a86507e6ad9effee0a280414
Jillian Guillen <jguillen@spartaglobal.com>
2019-10-05 14:58:42 +0100

    completed all user stories

--- run_file.py
+++ run_file.py
@@ -9,25 +9,25 @@ password = 'Passw0rd2018'
 conn_recipe = Recipe(server, database, username, password)
 
 #adding a recipe
-#conn_recipe.create_recipe(4, "'Nutella pancakes'", "'Flour, egg and nutella'", "'For the sweet tooth'", "'Mix flour, egg and nutella until sticky and fry'", "'HA55NZ'")
+conn_recipe.create_recipe(4, "'Nutella pancakes'", "'Flour, egg and nutella'", "'For the sweet tooth'", "'Mix flour, egg and nutella until sticky and fry'", "'HA55NZ'")
 
 #delete a query
-#conn_recipe.destroy_query("dbo.recipes", 4)
+conn_recipe.destroy_query("dbo.recipes", 4)
 
 #list all recipes
-#print(conn_recipe.list_all_recipes("dbo.recipes"))
+print(conn_recipe.list_all_recipes("dbo.recipes"))
 
 #read one object
-#print(conn_recipe.read_one_recipe("dbo.recipes", 3))
+print(conn_recipe.read_one_recipe("dbo.recipes", 3))
 
 #export recipe to a txt file
-#conn_recipe.write_one_recipe_to_txt(3)
+conn_recipe.write_one_recipe_to_txt(3)
 
 #update object
-#conn_recipe.update_recipe("dbo.recipes", "name", "'PB & J'", 2)
+conn_recipe.update_recipe("dbo.recipes", "name", "'PB & J'", 2)
 
 #more info on the location of the recipe where the recipe was created (postcode)
-#conn_recipe.recipe_info(3)
+conn_recipe.recipe_info(3)
 
 
 
